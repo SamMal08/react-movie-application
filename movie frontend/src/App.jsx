30f911dc21bbcbd12da3aea8964a60473550a589
SamMal08 <samaira140408@gmail.com>
2026-08-28 23:26:07 +0530

    context + favorites

--- movie frontend/src/App.jsx
+++ movie frontend/src/App.jsx
@@ -2,12 +2,13 @@
 import './css/App.css';
 import Favorites from './pages/Favorites';
 import Home from './pages/Home';
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route} from "react-router-dom";
+import { MovieProvider } from './contexts/MovieContext';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div>
+    <MovieProvider>
       <NavBar />
       <main className='main-content'>
         <Routes>
@@ -15,10 +16,8 @@ function App() {
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
-    </div>
+    </MovieProvider>
   );
 }
-
-
 
 export default App;
