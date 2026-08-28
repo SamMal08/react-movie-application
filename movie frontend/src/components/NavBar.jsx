4c7ebcbefa2bce15495e18a93a3beb68200bc679
SamMal08 <samaira140408@gmail.com>
2026-08-28 08:53:38 +0530

    Add CSS styling

--- movie frontend/src/components/NavBar.jsx
+++ movie frontend/src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "../css/NavBar.css"
 
 function NavBar() {
   return(
@@ -10,7 +11,7 @@ function NavBar() {
         <Link to="/" className="nav-link">Home</Link>
         <Link to="/favorites" className="nav-link">Favorites</Link>
       </div>
-    </nav>
+    </nav> 
   )
 }
 
