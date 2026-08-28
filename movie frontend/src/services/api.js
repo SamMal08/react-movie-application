10c05a373aedabc8fcd5c1e30653888390df2e1c
SamMal08 <samaira140408@gmail.com>
2026-08-28 21:21:51 +0530

    CSS + Search Button

---
+++ movie frontend/src/services/api.js
@@ -0,0 +1,17 @@
+const API_KEY = "80c6f6552c3d9c63c489e816ff3fb615";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json()
+  return data.results
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
+  );
+  const data = await response.json();
+  return data.results;
+
+};
